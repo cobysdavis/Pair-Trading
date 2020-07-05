b867bed8ef47b75385c97af69c484f4cb26ff48f
cobysdavis <cobysdavis@yahoo.ca>
2020-07-05 18:10:58 -0400

    fixed

--- Pair_Selection.py
+++ Pair_Selection.py
@@ -18,7 +18,7 @@ from statsmodels.tsa.stattools import adfuller as adf1
 import matplotlib.pyplot as plt
 plt.style.use('ggplot')
 
-def MeanReversionSpeed(data):  
+def MeanReversionSpeed(data):
     x=data[0:len(data)-1]
     y=data[1:len(data)]
     x=np.array(x)
@@ -45,14 +45,14 @@ plt.style.use('ggplot')
 
 
 #Set your path directory
-path="C:\\Users\\pulki\\OneDrive\Documents\\Algo Trading\\Data North America\\NYSE\\"
-files=os.listdir(path)
-
-
+urlUSA = 'https://raw.githubusercontent.com/cobysdavis/Pair-Trading/Pulkit/USA.csv'
+data = pd.read_csv(urlUSA, error_bad_lines=False)
+print(data.head())
 #load data for stocks and statistics
-data=pd.read_csv(path+"USA.csv")
-statistics=pd.read_csv(path+"Statistics.csv")
+urlStats = 'https://raw.githubusercontent.com/cobysdavis/Pair-Trading/Pulkit/Statistics.csv'
+statistics=pd.read_csv(urlStats,error_bad_lines=False)
 statistics.dropna(inplace=True)
+print(statistics.head())
 
 #Groupby sectors
 sectors_list=list(set(statistics.Sector))
@@ -71,7 +71,7 @@ for group in groups.keys():
     temp=[]
     print (group)
     for i in range(len(bins)):
-        for j in range(i+1,len(bins)):            
+        for j in range(i+1,len(bins)):
             beta1= bins.iloc[i].Beta
             beta2= bins.iloc[j].Beta
             marketCap1= bins.iloc[i].Marketcap
@@ -80,10 +80,10 @@ for group in groups.keys():
             mcDiff=abs(100*((marketCap1-marketCap2)/(marketCap1)))
             if betaDiff < 10 and mcDiff<10:
                 temp.append([bins.index[i],bins.index[j]])
-                
+
     pairsFilter1[group]=temp
-    
-    
+
+
 #Filter 3 based on Statistics
 pairsFilter2={}
 hurstThreshold=0.35
@@ -91,7 +91,7 @@ correlationThreshold=0.9
 for pairs in pairsFilter1.keys():
     bins=pairsFilter1[pairs]
     temp=[]
-    for stock in (bins):       
+    for stock in (bins):
         s1=data[stock[0]]
         s2=data[stock[1]]
         if (any([math.isnan(x) for x in s1])) or (any([math.isnan(x) for x in s2])):
@@ -105,8 +105,6 @@ for pairs in pairsFilter1.keys():
             corr=np.corrcoef(s1,s2)[0][1]
             if hurst1<hurstThreshold and corr>correlationThreshold:
                 temp.append(stock)
-                
-            
+
+
     pairsFilter2[pairs]=temp
-    
-    
